fe04d9563bc8362d046a55e3ea618259cf661079
CharlieLaplante <alexylaplante@hotmail.com>
2016-01-31 22:32:50 -0500

    sdasd

--- HtmlConverter/HtmlConverter/main.cpp
+++ HtmlConverter/HtmlConverter/main.cpp
@@ -2,49 +2,137 @@
 #include <map>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <iterator>
 using namespace std;
 
 
-bool AnalyseParameter(int argc, char* argv[], string param)
+
+bool FileExist(string filename)
 {
-	for (int i = 1; i < argc; i++)
+	ifstream infile(filename);
+	return infile.good();
+}
+
+template<class it, class T>
+bool AnalyseParameter(it debut,it end, const T &pred)
+{
+	for (; debut != end; ++debut)
 	{
-		if (argv[i] == "/" + param || argv[i] == "-" + param)
-		{
+		if (pred(*debut))
 			return true;
-		}
 	}
 	return false;
 }
 
-bool AnalyseFile(char* filename)
+template<class it, class T>
+bool FindFichier(it param, const T &pred)
 {
-	string exp = filename;
-	if (exp.substr(exp.find_first_of(".") + 1) == "cpp")
-	{
+	if (pred(*param) && FileExist(*param))
 		return true;
-	}
-	return false;
+	else
+		return false;
 }
 
+void convertirHtml(bool couleur,bool stat,ifstream& file, map<string, int> htmlMap)
+{
+	//build map
+	for (string s; file >> s;)
+		htmlMap[s]++;
+
+	for (auto &p : htmlMap)
+		cout << p.first << "/" << p.second << endl;
+}
 
 
 int main(int argc, char* argv[])
 {
-	if (argc > 1)
-	{		
-		map<string, int> mots;
+	vector<string> arguments(argv, argv + argc);
+	string listHtml[] = {
+		"<!DOCTYPE>","<a>",
+		"<abbr>","<acronym>",
+		"<address>","<applet>",
+		"<area>","<article>",
+		"<aside>","<audio>",
+		"<b>","<base>",
+		"<basefont>","<bdi>",
+		"<bdo>","<big>",
+		"<blockquote>",	"<body>",
+		"<br>","<button>",
+		"<canvas>","<caption>",
+		"<center>","<cite>",
+		"<code>","<col>",
+		"<colgroup>	","<command>",
+		"<datalist>	","<dd>",
+		"<del>","<details>",
+		"<dfn>","<dir>",
+		"<div>","<dl>",
+		"<dt>","<em>",
+		"<embed>","<fieldset>",
+		"<figcaption>",	"<figure>",
+		"<font>","<footer>",
+		"<form>","<frame>",
+		"<frameset>","<h1>",
+		"<h2>","<h3>",
+		"<h4>","<h5>",
+		"<h6>","<head>",
+		"<header>","<hgroup>",
+		"<hr>","<html>",
+		"<i>","<iframe>",
+		"<img>","<input>",
+		"<ins>","<kbd>",
+		"<keygen>","<label>",
+		"<legend>","<li>",
+		"<link>","<map>",
+		"<mark>","<menu>",
+		"<meta>","<meter>",
+		"<nav>","<noframes>",
+		"<noscript>","<object>",
+		"<ol>","<optgroup>",
+		"<option>","<output>",
+		"<p>","<param>",
+		"<pre>","<progress>",
+		"<q>","<rp>",
+		"<rt>","<ruby>",
+		"<s>","<samp>",
+		"<script>","<section>",
+		"<select>","<small>",
+		"<source>","<span>",
+		"<strike>","<strong>",
+		"<style>","<sub>",
+		"<summary>","<sup>",
+		"<table>","<tbody>",
+		"<td>","<textarea>",
+		"<tfoot>","<th>",
+		"<thead>","<time>",
+		"<title>","<tr>",
+		"<track>","<tt>",
+		"<u>","<ul>",
+		"<var>","<video>",
+		"<wbr>" };
+				
+	//Enlever premier argument qui est le nom de l'executable
+	arguments.erase(arguments.begin());
+	if (!arguments.empty())
+	{	
+		//Check les paramètres
+		bool couleur = AnalyseParameter(begin(arguments), end(arguments), [](string param) {return param == "/couleur" || param == "-couleur"; });
+		bool stat = AnalyseParameter(begin(arguments), end(arguments), [](string param) {return param == "/stat" || param == "-stat"; });
+		
+		//la map html
+		map<string, int> htmlMap;
 
-		for (int i = 1; i < argc; ++i)
-		{
-			if (AnalyseFile(argv[i]))
+		//ce promène dans la liste d'argument pour trouver les fichiers qui existe et qui respectent le predicat
+		vector<string>::iterator it = begin(arguments);
+		for (string filename;it!=end(arguments);it++)
+		{		   
+			if (FindFichier(it, [](string param) {return param.substr(param.find_last_of(".") + 1) == "cpp"; }))
 			{
-				//ouvre le premier fichier trouver dans les parametres
-				ifstream in(argv[i]);
-
-			}
-		}
-			
+				filename = *it;	
+				ifstream in(filename);
+				convertirHtml(couleur,stat,in,htmlMap);
+			}			
+		}	
 	}
 	else
 	{
