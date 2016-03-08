87fe86b4e1a515a9192eddeaf83a1825cbcfb0a3
Brosseau <xavierb.2006@hotmail.com>
2016-03-07 20:58:57 -0500

    update 20

--- HtmlConverter/HtmlConverter/main.cpp
+++ HtmlConverter/HtmlConverter/main.cpp
@@ -17,7 +17,7 @@ using namespace chrono;
 
 
 
-bool FileExist(string &filename) 
+bool FileExist(string &filename)
 {
 	return ifstream{ filename }.good();
 }
@@ -25,7 +25,7 @@ bool FileExist(string &filename)
 template<class it, class T>
 bool AnalyseParameter(it debut, it fin, const T &pred)
 {
-	return find_if(debut,fin,pred)!=fin;
+	return find_if(debut, fin, pred) != fin;
 }
 
 template<class it, class T>
@@ -40,7 +40,7 @@ void convertirHtml(bool couleur, bool stat, map<string, int> htmlMap, string fil
 	vector<string> FileTemp;
 	for (string s; getline(file, s);)
 		FileTemp.push_back(s);
-	
+
 
 	ofstream myHtmlFile;
 	myHtmlFile.open(filename + ".html");//Création du fichier
@@ -61,7 +61,7 @@ void convertirHtml(bool couleur, bool stat, map<string, int> htmlMap, string fil
 	{
 		file.open(filename);
 		ofstream myfile;
-		myfile.open(filename+"Stats.txt");
+		myfile.open(filename + "Stats.txt");
 		//build map
 		for (string s; file >> s;)
 			htmlMap[s]++;
@@ -152,93 +152,97 @@ void convertirHtml(bool couleur, bool stat, map<string, int> htmlMap, string fil
 int main(int argc, char* argv[])
 {
 	const int nbCoeur = thread::hardware_concurrency();
-		
-
-	vector<string> arguments(argv + 1, argv + argc);
-	//Forcer des arguments ICI
-	//-------------------------
-	arguments.push_back("-stat");
-	arguments.push_back("-couleur");
-	arguments.push_back("fuck.cpp");
-	arguments.push_back("fuck - Copie.cpp");
-	for (int i = 2; i < 64; i++)
+	for (int NbFichier = 1; NbFichier <= 30; ++NbFichier)
 	{
-		arguments.push_back("fuck - Copie ("+to_string(i)+").cpp");
-	}
 
-	if (!arguments.empty())
-	{
-		//Check les paramètres
-		bool couleur = AnalyseParameter(begin(arguments),
-			end(arguments),
-			[](string param) {return param == "/couleur" || param == "-couleur"; });
-		bool stat = AnalyseParameter(begin(arguments),
-			end(arguments),
-			[](string param) {return param == "/stat" || param == "-stat"; });
-	
-		//la map html
-		map<string, int> htmlMap;
-		
-	
-		//Sequenciel
-		//ce promène dans la liste d'argument pour trouver les fichiers qui existe et qui respectent le predicat
-		vector<string>::iterator it = begin(arguments);
-		high_resolution_clock::time_point tempSequenceDebut = high_resolution_clock::now();
-		for (string filename; it != end(arguments); it++)
+		vector<string> arguments(argv + 1, argv + argc);
+		//Forcer des arguments ICI
+		//-------------------------
+		arguments.push_back("-stat");
+		arguments.push_back("-couleur");
+		/*arguments.push_back("fuck.cpp");
+		if (NbFichier >= 2)
+			arguments.push_back("fuck - Copie.cpp");*/
+		for (int i = 0/*2*/; i < NbFichier; i++)
 		{
-			if (FindFichier(it, [](string param) {string str = param.substr(param.find_last_of(".") + 1);
-			for (auto & c : str) c = toupper(c); return  str == "CPP"; }))
+			//arguments.push_back("fuck - Copie (" + to_string(i) + ").cpp");
+			arguments.push_back("fuck.cpp");
+		}
+
+		if (!arguments.empty())
+		{
+			//Check les paramètres
+			bool couleur = AnalyseParameter(begin(arguments),
+				end(arguments),
+				[](string param) {return param == "/couleur" || param == "-couleur"; });
+			bool stat = AnalyseParameter(begin(arguments),
+				end(arguments),
+				[](string param) {return param == "/stat" || param == "-stat"; });
+
+			//la map html
+			map<string, int> htmlMap;
+
+
+			//Sequenciel
+			//ce promène dans la liste d'argument pour trouver les fichiers qui existe et qui respectent le predicat
+			vector<string>::iterator it = begin(arguments);
+			high_resolution_clock::time_point tempSequenceDebut = high_resolution_clock::now();
+			for (string filename; it != end(arguments); it++)
 			{
-					filename = *it;	
+				if (FindFichier(it, [](string param) {string str = param.substr(param.find_last_of(".") + 1);
+				for (auto & c : str) c = toupper(c); return  str == "CPP"; }))
+				{
+					filename = *it;
 					convertirHtml(couleur, stat, htmlMap, filename);
+				}
 			}
-		}
-		high_resolution_clock::time_point tempSequenceFin = high_resolution_clock::now();
-		duration<double> time_span_Seq = duration_cast<duration<double>>(tempSequenceFin - tempSequenceDebut);
+			high_resolution_clock::time_point tempSequenceFin = high_resolution_clock::now();
+			duration<double> time_span_Seq = duration_cast<duration<double>>(tempSequenceFin - tempSequenceDebut);
 
-		//Parrallele
-		vector<thread> lesThreads;
-		for (int i = 0; i < nbCoeur; ++i)
-		{
-			lesThreads.push_back(thread());
-		}
-	    it = begin(arguments);
-
-		high_resolution_clock::time_point tempParaDebut = high_resolution_clock::now();
-		int i = 0;
-		for (string filename; it != end(arguments); it++)
-		{
-			if (FindFichier(it, [](string param) {string str = param.substr(param.find_last_of(".") + 1); for (auto & c : str)
-				c = toupper(c); return  str == "CPP"; }))
+			//Parrallele
+			vector<thread> lesThreads;
+			for (int i = 0; i < nbCoeur; ++i)
 			{
-				filename = *it;
-				int tempo = i%nbCoeur;
-				if (lesThreads[tempo].joinable())
-					lesThreads[tempo].join();
-				lesThreads[tempo] = thread(convertirHtml, couleur, stat, htmlMap, filename);
-				//lesThreads.push_back(t1);
+				lesThreads.push_back(thread());
 			}
-			++i;
-		}
-		for (int i = 0; i < nbCoeur; ++i)
-		{
-			if (lesThreads[i].joinable())
-				lesThreads[i].join();
-		}
-		high_resolution_clock::time_point tempParaFin = high_resolution_clock::now();
-		duration<double> time_span_Para = duration_cast<duration<double>>(tempParaFin - tempParaDebut);
+			it = begin(arguments);
 
-		ofstream statfile("statfichier.txt",ios::app);
-		statfile << "Nombre de coeur" << nbCoeur << endl;
-		statfile << "Pour " << arguments.size()-2 << " fichier"<<endl;
-		statfile << "Sequenciel : " << time_span_Seq.count()<<endl;
-		statfile << "Parralelle : " << time_span_Para.count() << endl;
-		statfile << "--------------------------------------------------------------------" << endl;
-		
-	
-	}
-	else
-	{
-		cout << "vous avez entre aucun parametre" << endl;
+			high_resolution_clock::time_point tempParaDebut = high_resolution_clock::now();
+			int i = 0;
+			for (string filename; it != end(arguments); it++)
+			{
+				if (FindFichier(it, [](string param) {string str = param.substr(param.find_last_of(".") + 1); for (auto & c : str)
+					c = toupper(c); return  str == "CPP"; }))
+				{
+					filename = *it;
+					int tempo = i%nbCoeur;
+					if (lesThreads[tempo].joinable())
+						lesThreads[tempo].join();
+					lesThreads[tempo] = thread(convertirHtml, couleur, stat, htmlMap, filename);
+					//lesThreads.push_back(t1);
+				}
+				++i;
+			}
+			for (int i = 0; i < nbCoeur; ++i)
+			{
+				if (lesThreads[i].joinable())
+					lesThreads[i].join();
+			}
+			high_resolution_clock::time_point tempParaFin = high_resolution_clock::now();
+			duration<double> time_span_Para = duration_cast<duration<double>>(tempParaFin - tempParaDebut);
+
+			ofstream statfile("statfichier.txt", ios::app);
+			/*statfile << "Nombre de coeur" << nbCoeur << endl;
+			statfile << "Pour " << arguments.size()-2 << " fichier"<<endl;
+			statfile << "Sequenciel : " << time_span_Seq.count()<<endl;
+			statfile << "Parralelle : " << time_span_Para.count() << endl;
+			statfile << "--------------------------------------------------------------------" << endl;*/
+			statfile << time_span_Seq.count() << "\t\t" << time_span_Para.count() << endl;
+
+		}
+		else
+		{
+			cout << "vous avez entre aucun parametre" << endl;
+		}
 	}
 }
